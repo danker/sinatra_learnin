b5b3e427ed5bcfad8b815ca4c9ec22d52388322f
Eric J. Danker <eric@breomedia.com>
2012-07-15 13:27:51 -0700

    Adding basic views and form processing.

--- basics.rb
+++ basics.rb
@@ -7,3 +7,33 @@ end
 get '/about' do
     "A little about me..."
 end
+
+get '/hello/:name/:city' do
+    "What up #{params[:name]} from #{params[:city]}."
+end
+
+get '/form' do
+    erb :form
+end
+
+post '/form' do
+    "You said '#{params[:message]}'"
+end
+
+get '/secret' do
+    erb :secret
+end
+
+post '/secret' do
+    params[:secret].reverse
+end
+
+get '/decrypt/:secret' do
+    params[:secret].reverse
+end
+
+# -------------------------------------
+not_found do
+    status 404
+    'not found'
+end
